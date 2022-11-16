73eb9d8ecf94aa5fdc417b80f293705880251ff2
Shelby Stevens <shelbylynnstevens@gmail.com>
2022-11-15 17:56:27 -0800

    wk 3 wrkshp

---
+++ src/features/campsites/CampsitesList.js
@@ -0,0 +1,21 @@
+import { selectAllCampsites } from './campsitesSlice';
+import { Col, Row } from 'reactstrap';
+import CampsiteCard from './CampsiteCard';
+import CommentForm from '../comments/CommentForm';
+
+const CampsitesList = () => {
+    const campsites = selectAllCampsites();
+    return (
+        <Row className='ms-auto'>
+            {campsites.map((campsite) => {
+                return (
+                    <Col md='5' className='m-4' key={campsite.id}>
+                        <CampsiteCard campsite={campsite} />
+                    </Col>
+                );
+            })}
+        </Row>
+    );
+};
+
+export default CampsitesList;
